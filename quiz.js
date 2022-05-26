a7502ded115c6809216226aed66b17b8d1795377
td0428 <rin408211@gmail.com>
2022-05-27 08:45:46 +0900

    JavaScriptでクイズを作る

--- quiz.js
+++ quiz.js
@@ -1,20 +1,25 @@
+const seikai = "正解"
+const huseikai = "不正解"
+
+
 document.getElementById('answerA').addEventListener('click', function (){
-    this.style.backgroundColor="gray"
-    this.textContent="不正解"
+    // this.style.backgroundColor="gray"
+    this.textContent= huseikai
     this.style.color="blue"
+    this.setAttribute('class','huseikai')
 })
 document.getElementById('answerB').addEventListener('click', function (){
     this.style.backgroundColor="gray"
-    this.textContent="不正解"
+    this.textContent=huseikai
     this.style.color="blue"
 })
 document.getElementById('answerC').addEventListener('click', function (){
     this.style.backgroundColor="skyblue"
-    this.textContent="正解"
+    this.textContent=seikai
     this.style.color="red"
 })
 document.getElementById('answerD').addEventListener('click', function (){
     this.style.backgroundColor="gray"
-    this.textContent="不正解"
+    this.textContent=huseikai
     this.style.color="blue"
 })
