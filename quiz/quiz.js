5a93827df110903e3df0dff7969b09c8d6797c20
td0428 <rin408211@gmail.com>
2022-06-06 13:15:14 +0900

    for文を使って少し変更

--- quiz/quiz.js
+++ quiz/quiz.js
@@ -1,3 +1,18 @@
+
+// 選択肢の配列
+const answers = ["大阪","愛知","東京","福岡"];
+
+// 選択肢のIDの配列
+const answerIDs = ["answerA","answerB","answerC","answerD"];
+
+for (let i = 0;i<4;i++){
+    document.getElementById(answerIDs[i]).textContent = answers[i];
+    console.log(answerIDs[i]);
+    console.log(answers[i]);
+}
+
+
+
 const seikai = "正解"
 const huseikai = "不正解"
 
