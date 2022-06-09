8e16c5848f72de0b1efa24609c64b0196f2b48aa
td0428 <rin408211@gmail.com>
2022-06-09 14:10:46 +0900

    引数を使ってまとめる

--- quiz/quiz.js
+++ quiz/quiz.js
@@ -5,7 +5,7 @@ const answers = ["大阪","愛知","東京","福岡"];
 // 選択肢のIDの配列
 const answerIDs = ["answerA","answerB","answerC","answerD"];
 
-for (let i = 0;i<4;i++){
+for (let i = 0;i < 4;i++){
     document.getElementById(answerIDs[i]).textContent = answers[i];
     console.log(answerIDs[i]);
     console.log(answers[i]);
@@ -14,26 +14,60 @@ for (let i = 0;i<4;i++){
 
 
 const seikai = "正解"
-const huseikai = "不正解"
+const fuseikai = "不正解"
+
+// 引数idNameを利用して、A,B,Dを当てはめる
+const makeFunction = function(idName){
+
+    document.getElementById(idName).addEventListener('click', function (){
+        this.style.backgroundColor="gray"
+        this.textContent= fuseikai
+        this.style.color="blue"
+        this.setAttribute("class","fuseikai")
+    })
+}
 
 
-document.getElementById('answerA').addEventListener('click', function (){
-    this.style.backgroundColor="gray"
-    this.textContent= huseikai
-    this.style.color="blue"
-})
-document.getElementById('answerB').addEventListener('click', function (){
-    this.style.backgroundColor="gray"
-    this.textContent=huseikai
-    this.style.color="blue"
-})
-document.getElementById('answerC').addEventListener('click', function (){
-    this.style.backgroundColor="skyblue"
-    this.textContent=seikai
-    this.style.color="red"
-})
-document.getElementById('answerD').addEventListener('click', function (){
-    this.style.backgroundColor="gray"
-    this.textContent=huseikai
-    this.style.color="blue"
-})
+const makeSeikai = function(idName){
+
+    document.getElementById(idName).addEventListener('click', function (){
+        this.style.backgroundColor="skyblue"
+        this.textContent=seikai
+        this.style.color="red"
+    })
+}
+
+
+// 関数の実行
+makeFunction("answerA")
+makeFunction("answerB")
+makeSeikai("answerC")
+makeFunction("answerD")
+
+
+// document.getElementById('answerA').addEventListener('click', function (){
+//     this.style.backgroundColor="gray"
+//     this.textContent= fuseikai
+//     this.style.color="blue"
+//     this.setAttribute("class","fuseikai")
+// })
+
+// document.getElementById('answerB').addEventListener('click', function (){
+//     this.style.backgroundColor="gray"
+//     this.textContent=fuseikai
+//     this.style.color="blue"
+//     this.setAttribute("class","fuseikai")
+// })
+
+// document.getElementById('answerC').addEventListener('click', function (){
+//     this.style.backgroundColor="skyblue"
+//     this.textContent=seikai
+//     this.style.color="red"
+// })
+
+// document.getElementById('answerD').addEventListener('click', function (){
+//     this.style.backgroundColor="gray"
+//     this.textContent=fuseikai
+//     this.style.color="blue"
+//     this.setAttribute("class","fuseikai")
+// })
